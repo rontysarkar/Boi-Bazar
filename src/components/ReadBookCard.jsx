3f2d4a28505e383dc11ded3dccf503660bd4467c
rontysarkar <rontysarkar2023@gmail.com>
2024-03-26 21:19:03 +0600

    add Read card click funcainality

--- src/components/ReadBookCard.jsx
+++ src/components/ReadBookCard.jsx
@@ -1,41 +1,51 @@
 import { IoPersonOutline } from "react-icons/io5";
 import { MdInsertPageBreak } from "react-icons/md";
 import { CiLocationOn } from "react-icons/ci";
+import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
 
-const ReadBookCard = () => {
+const ReadBookCard = ({ book }) => {
+    const { bookId, bookName, author, image, tags, category, rating,  yearOfPublishing, totalPages, publisher } = book;
     return (
         <div className=" border p-2 mt-10 rounded-lg  ">
             <div className="hero-content justify-start items-start flex-col lg:flex-row">
-                <div className='bg-base-200 rounded-xl w-[300px] h-[300px]'>
-
+                <div className='bg-base-200 rounded-xl w-[300px] h-[300px] flex items-center justify-center'>
+                    <img className="w-3/4 " src={image} alt="" />
                 </div>
                 <div className="space-y-5">
-                    <h1 className="text-3xl font-bold">The Catcher in the Rye</h1>
-                    <p className="text-xl">By : Awlad Hossain</p>
+                    <h1 className="text-3xl font-bold">{bookName}</h1>
+                    <p className="text-xl">By : {author}</p>
                     <div className="flex items-center gap-6 ">
-                        <div className="flex items-center gap-4">
-                            <h1 className="font-bold text-xl">Tag</h1>
-                            <button  className='px-2 py-1  bg-[#f4fcf3] rounded-full text-[#23be0a]'>comdy</button>
-                            <button  className='px-2 py-1  bg-[#f4fcf3] rounded-full text-[#23be0a]'>comdy</button>
+                        <div className="flex gap-4 ">
+                            <div className="flex items-center gap-4 ">
+                                <h1 className="text-xl font-bold">Tag</h1>
+                                {
+                                    tags.map(tag => <button key={tag} className='px-2 py-1  bg-[#f4fcf3] rounded-full text-[#23be0a]'>{tag}</button>)
+                                }
+                            </div>
                         </div>
                         <div>
-                            <h1 className="text-xl flex items-center gap-2"><CiLocationOn /> Year of Publishing: 1924</h1>
+                            <h1 className="text-xl flex items-center gap-2"><CiLocationOn /> Year of Publishing:{yearOfPublishing}</h1>
                         </div>
                     </div>
                     <div className="flex items-center gap-8 text-xl">
-                        <h1 className="flex items-center gap-2"> <IoPersonOutline /> Publisher: Scribner</h1>
-                        <p className="flex items-center gap-2"> <MdInsertPageBreak />Page 192</p>
+                        <h1 className="flex items-center gap-2"> <IoPersonOutline /> Publisher:  {publisher}</h1>
+                        <p className="flex items-center gap-2"> <MdInsertPageBreak />Page  {totalPages}</p>
                     </div>
-                    <hr  />
+                    <hr />
                     <div className="space-x-5">
-                    <button  className='px-4 py-2  bg-[#e0eeff] rounded-full text-[#338eff]'>Category: Classic</button>
-                    <button  className='px-4 py-2  bg-[#fff3e1] rounded-full text-[#ffc774]'>Category: Classic</button>
-                    <button  className='px-4 py-2  bg-green-500 rounded-full text-white'>Category: Classic</button>
+                        <button className='px-4 py-2  bg-[#e0eeff] rounded-full text-[#338eff]'>Category: {category}</button>
+                        <button className='px-4 py-2  bg-[#fff3e1] rounded-full text-[#ffc774]'>Category: {rating}</button>
+                        <Link to={`/bookDetails/${bookId}`} ><button className='px-4 py-2  bg-green-500 rounded-full text-white'>View Details</button></Link>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
+ReadBookCard.propTypes = {
+    book: PropTypes.object
+}
 
 export default ReadBookCard;
--- src/components/ReadBooks.jsx
+++ src/components/ReadBooks.jsx
@@ -1,10 +1,20 @@
+
+import { getReadInfo } from "../utility/getReadInfo";
 import ReadBookCard from "./ReadBookCard";
 
 
 const ReadBooks = () => {
+    
+    const localStorageReadData = getReadInfo();
+    
+    
+    
     return (
         <div>
-            <ReadBookCard/>
+            
+            {
+                localStorageReadData.map(book=><ReadBookCard key={book.bookId} book={book} />)
+            }
         </div>
     );
 };
